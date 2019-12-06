6b79d432a8d760ad3dcf8255c38d229e4971d7b8
Gravitar64 <gravitar@web.de>
2019-12-06 21:35:15 +0100

    leichte optimierungen

--- AoC_Tag 06.py
+++ AoC_Tag 06.py
@@ -1,23 +1,21 @@
 from collections import defaultdict
 import time
 time_start = time.perf_counter()
-#orbits = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L".split()
-
-with open('tag06.txt') as f:
-  orbits = f.read().splitlines()
 
 planeten = defaultdict(list)
-for orbit in orbits:
-  planet, satellit = orbit.split(')')
-  planeten[planet].append(satellit)
+with open('tag06.txt') as f:
+  for line in f.readlines():
+    planet, satellit = line.strip().split(')')
+    planeten[planet].append(satellit)
 
-orbit_counts = {}
-def count_orbits(node, counter):
-  orbit_counts[node] = counter
+
+def count_orbits(node, counter, total_sum):
+  total_sum += counter
   for satellit in planeten[node]:
-    count_orbits(satellit,counter+1)
+    total_sum = count_orbits(satellit,counter+1, total_sum)
+  return total_sum  
+    
 
-count_orbits('COM', 0)
-print(sum(orbit_counts.values()))
+print(count_orbits('COM', 0, 0))
 print(time.perf_counter() - time_start)
   
--- AoC_Tag 06a.py
+++ AoC_Tag 06a.py
@@ -1,20 +1,17 @@
 from collections import defaultdict
 import time
 time_start = time.perf_counter()
-#orbits = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN".split()
 
+nodes = defaultdict(list)
 with open('tag06.txt') as f:
-  orbits = f.read().splitlines()
-
-planeten = defaultdict(list)
-for orbit in orbits:
-  planet, satellit = orbit.split(')')
-  planeten[planet].append(satellit)
-  planeten[satellit].append(planet)
-  if satellit == "YOU":
-    start = planet
-  if satellit == "SAN":
-    ziel = planet
+  for line in f.readlines():
+    von, zu = line.strip().split(')')
+    nodes[von].append(zu)
+    nodes[zu].append(von)
+    if zu == "YOU":
+      start = von
+    if zu == "SAN":
+      ziel = von
 
 besucht = set()
 def find_way(node, counter):
@@ -22,10 +19,10 @@ def find_way(node, counter):
   if node == ziel:
     print(counter)
     return True
-  for next_node in planeten[node]:
+  for next_node in nodes[node]:
     if next_node in besucht: continue
     if find_way(next_node, counter+1):
-      return True  
+      return True 
   
 
 find_way(start,0)
