adc2bbfb5503e9861c683c54691cd20e15ebcb7d
Gravitar64 <gravitar@web.de>
2019-12-13 23:45:46 +0100

    noch mehr redundanzen eleminiert

--- AoC_Tag 12a.py
+++ AoC_Tag 12a.py
@@ -23,29 +23,24 @@ with open('Tag12.txt') as f:
 lösungen = []  
 
 for achsen_zähler in range(3):
-  moons = []
-  for pos in puzzle_input:
-    moons.append(Moon(pos))
-  start_state = tuple(moon.state(achsen_zähler) for moon in moons)
+  moons = [Moon(pos) for pos in puzzle_input]
+  start_state = set(moon.state(achsen_zähler) for moon in moons)
   fertig = False
   cycles = 0
   while not fertig:
     cycles += 1
     for moon1 in moons:
+      vel_list = list(moon1.vel)
       for moon2 in moons:
         if moon1 == moon2: continue
         diff = moon2.pos - moon1.pos
-        vel_list = list(moon1.vel)
         for i,achse in enumerate(diff):
           if achse == 0: continue
-          if achse > 0:
-            vel_list[i] += 1
-          if achse < 0:
-            vel_list[i] -= 1
-        moon1.vel = Vec(*vel_list)    
+          vel_list[i] += 1 if achse > 0 else -1            
+      moon1.vel = Vec(*vel_list)    
     for moon in moons:
       moon.pos += moon.vel
-    state = tuple(moon.state(achsen_zähler) for moon in moons)
+    state = set(moon.state(achsen_zähler) for moon in moons)
     if state == start_state:
       fertig = True
       lösungen.append(cycles)
