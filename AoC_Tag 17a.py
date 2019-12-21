fb56b30cf62b4c98d9e665124fa42eb2ad641295
Gravitar64 <gravitar@web.de>
2019-12-21 11:29:02 +0100

    youtube version teil 1

--- AoC_Tag 17a.py
+++ AoC_Tag 17a.py
@@ -5,8 +5,8 @@ from collections import defaultdict, deque
 from Vector import Vec
 import time
 
-# ^, v, <, or > for directions
-richtungen = {'^': Vec(0, -1), 'v': Vec(0, 1), '<': Vec(-1, 0), '>': Vec(1, 0)}
+#^, v, <, or > for directions
+richtungen = {'^':Vec(0,-1), 'v':Vec(0,1), '<':Vec(-1,0), '>':Vec(1,0)}
 
 
 class Intcode:
@@ -14,13 +14,8 @@ class Intcode:
     self.mem = defaultdict(int, enumerate(program))
     self.ip = 0
     self.rb = 0
-    self.input = []
-    self.output = []
 
-  def load_inputs(self, data):
-    self.input.extend(data)
-
-  def run(self):
+  def run(self, program_input):
     while True:
       op = self.mem[self.ip] % 100
       if op == 99:
@@ -28,8 +23,7 @@ class Intcode:
       size = [0, 4, 4, 2, 2, 3, 3, 4, 4, 2][op]
       args = [self.mem[self.ip+i] for i in range(1, size)]
       modes = [(self.mem[self.ip] // 10 ** i) % 10 for i in range(2, 5)]
-      reads = [(self.mem[x], x, self.mem[x+self.rb])[m]
-               for x, m in zip(args, modes)]
+      reads = [(self.mem[x], x, self.mem[x+self.rb])[m] for x, m in zip(args, modes)]
       writes = [(x, None, x+self.rb)[m] for x, m in zip(args, modes)]
       self.ip += size
       if op == 1:
@@ -37,9 +31,9 @@ class Intcode:
       if op == 2:
         self.mem[writes[2]] = reads[0] * reads[1]
       if op == 3:
-        self.mem[writes[0]] = self.input.pop(0)
+        self.mem[writes[0]] = program_input
       if op == 4:
-        self.output.append(reads[0])
+        return reads[0]
       if op == 5 and reads[0]:
         self.ip = reads[1]
       if op == 6 and not reads[0]:
@@ -49,31 +43,24 @@ class Intcode:
       if op == 8:
         self.mem[writes[2]] = int(reads[0] == reads[1])
       if op == 9:
-        self.rb += reads[0]
-
-
+        self.rb += reads[0]  
+    
 start = time.perf_counter()
-
 with open('Tag17.txt') as f:
   program = list(map(int, f.readline().split(',')))
 
-
-def baue_map(data):
-  karte = set()
-  pos = Vec(0, 0)
-  for element in data:
-    e = chr(element)
-    if e in richtungen:
-      robot = pos, e
-    if e == '#':
-      karte.add(pos)
-    x, y = pos
+def baue_map(pos):
+  map = set()  
+  while True:
+    ascii = intcode.run(0)
+    if ascii == None: return map, robot
+    if chr(ascii) in richtungen:
+      robot = pos, chr(ascii)
+    if ascii == 35: map.add(pos)
+    x,y = pos
     pos = Vec(x+1, y)
-    if e == '\n':
-      pos = Vec(0, y+1)
-  return karte, robot
-
-
+    if ascii == 10: pos = Vec(0, y+1)   
+  return map
 def finde_neue_richtung(pos, richt):
   x, y = richt
   for drehung in ['L', 'R']:
@@ -81,10 +68,8 @@ def finde_neue_richtung(pos, richt):
       x1, y1 = y*1, x*-1
     else:
       x1, y1 = y*-1, x * 1
-    if pos + Vec(x1, y1) in karte:
+    if pos + Vec(x1, y1) in map:
       return drehung, Vec(x1, y1)
-
-
 def run_scaffold(map, robot):
   pos, dir_indicator = robot
   richt_delta = richtungen[dir_indicator]
@@ -106,56 +91,9 @@ def run_scaffold(map, robot):
   return(movement_commands)
 
 
-def generate_inputs(patterns, enc_str, trans):
-  trans_invers = {v: k for k, v in trans.items()}
-  main_routine = enc_str.replace(patterns[0], 'A,').replace(
-      patterns[1], 'B,').replace(patterns[2], 'C,')[:-1]+'\n'
-  for i in range(3):
-    main_routine += ','.join([trans_invers[a][:1]+',' +
-                              trans_invers[a][1:] for a in patterns[i]])+'\n'
-  return main_routine + 'n\n'
-
-
-def find_functions(moves):
-  mov = [moves[i]+moves[i+1] for i in range(0, len(moves), 2)]
-  mov_str = mov_enc_str = ''.join(mov)
-  mov_trans = {}
-  enc = 75
-  for move in mov:
-    if move in mov_trans:
-      continue
-    mov_trans[move] = chr(enc)
-    enc += 1
-  for old, new in mov_trans.items():
-    mov_enc_str = mov_enc_str.replace(old, new)
-  while True:
-    for i in range(1, 7):
-      move_str = mov_enc_str
-      a = move_str[:i]
-      c = move_str[-i:]
-      move_str = move_str.replace(a, '').replace(c, '')
-      b = move_str
-      for j in range(1, len(b)):
-        b_pat = b.replace(b[:j], '')
-        if b_pat == '':
-          program_inputs = generate_inputs(
-              (a, b[:j], c), mov_enc_str, mov_trans)
-    break
-  return program_inputs
-
-
 intcode = Intcode(program)
-intcode.run()
-karte, robot = baue_map(intcode.output)
+map, robot = baue_map(Vec(0,0))
+movements = run_scaffold(map, robot)
+print(movements)
 
-program[0] = 2
-intcode = Intcode(program)
-moves = run_scaffold(karte, robot)
-inputs = find_functions(moves)
-intcode.load_inputs([ord(x) for x in inputs])
-intcode.run()
-# for item in intcode.output:
-#   print(chr(item), end='')
-lösung = intcode.output[-1]
-
-print(f'Lösung = {lösung} in {time.perf_counter()-start} Sek.')
+#print(f'Lösung = {lösung} in {time.perf_counter()-start} Sek.')
