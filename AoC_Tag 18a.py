46e65ec10402cc63c83e191d596658639107f520
Gravitar64 <gravitar@web.de>
2019-12-26 15:09:26 +0100

    youtube version

--- AoC_Tag 18a.py
+++ AoC_Tag 18a.py
@@ -1,12 +1,13 @@
 from collections import deque
 
-with open('tag18.txt') as f:
+with open('tag18_2.txt') as f:
   walls = set()
   keys, dors = {}, {}
+  starts = []
   for y,zeile in enumerate(f):
     for x, char in enumerate(zeile.strip()):
       if char == '@':
-        start_pos = (x,y)
+        starts.append((x,y))
       elif char == '#':
         walls.add((x,y))
       elif char.islower():
@@ -37,20 +38,28 @@ def erreichbare_schlüssel(pos, gesammelte_schlüssel):
         stack.append(nachb)
   return erreichbareSchlüssel
 
+def wähleStartaus4(starts, gesammelte_schlüssel):
+  erreichbareSchlüssel = {}
+  for rob_id, start in enumerate(starts):
+    for char, (dist, pos) in erreichbare_schlüssel(start, gesammelte_schlüssel).items():
+      erreichbareSchlüssel[char]= dist, pos, rob_id
+  return erreichbareSchlüssel      
+
 cache = {}
-def kürzester_weg(start, gesammelte_schlüssel):
+def kürzester_weg(starts, gesammelte_schlüssel):
   cache_str = ''.join(sorted(gesammelte_schlüssel))
-  if (start,cache_str) in cache:
-    return cache[start,cache_str]
-  erreichbareSchlüssel = erreichbare_schlüssel(start, gesammelte_schlüssel)
+  if (starts,cache_str) in cache:
+    return cache[starts,cache_str]
+  erreichbareSchlüssel = wähleStartaus4(starts, gesammelte_schlüssel)
   if not erreichbareSchlüssel:
     erg = 0
   else:
     ergebnisse = []
-    for char, (dist, pos) in erreichbareSchlüssel.items():
-      ergebnisse.append(dist + kürzester_weg(pos, gesammelte_schlüssel+char))
+    for char, (dist, pos, rob_id) in erreichbareSchlüssel.items():
+      nstarts = tuple(pos if i == rob_id else p for i,p in enumerate(starts))
+      ergebnisse.append(dist + kürzester_weg(nstarts, gesammelte_schlüssel+char))
     erg = min(ergebnisse)
-  cache[start,cache_str] = erg  
+  cache[starts,cache_str] = erg  
   return erg
 
-print(kürzester_weg(start_pos,''))        
+print(kürzester_weg(tuple(starts),''))        
